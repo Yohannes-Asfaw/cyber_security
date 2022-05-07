7bd7e1d33591a3cb6662fb8e218581072b3c17ff
Yohannes-Asfaw <jonathanasfaw11@gmail.com>
2022-05-07 16:04:11 +0300

    tikinter gui for input and output

--- cipher_encription.py
+++ cipher_encription.py
@@ -12,3 +12,37 @@ def cipher(str1):
         else:
             str2 += chr(ord(i) + jump)
     return str2
+import tkinter as tk
+
+# Top level window
+frame = tk.Tk()
+frame.title("TextBox Input")
+frame.geometry('400x200')
+
+
+# Function for getting Input
+# from textbox and printing it
+# at label widget
+
+def printInput():
+    inp = inputtxt.get(1.0, "end-1c")
+    lbl.config(text="Provided Input: " + inp)
+
+
+# TextBox Creation
+inputtxt = tk.Text(frame,
+                   height=5,
+                   width=20)
+
+inputtxt.pack()
+
+# Button Creation
+printButton = tk.Button(frame,
+                        text="Print",
+                        command=printInput)
+printButton.pack()
+
+# Label Creation
+lbl = tk.Label(frame, text="")
+lbl.pack()
+frame.mainloop()
